3e9005572860295cf73f43cdb6df48e36e753ab8
innerchen <innerchen@outlook.com>
2020-12-04 19:18:05 +0800

    Bresenham’s Line

--- main.cpp
+++ main.cpp
@@ -3,11 +3,55 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+
+    bool steep = false;
+    if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+
+    if (x0 > x1) {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+
+    int dx = x1 - x0;
+    int dy = std::abs(y1 - y0);
+
+    int error = dx / 2;
+
+    int y = y0;
+    for (int x = x0; x <= x1; ++x) {
+
+        if (steep) {
+            image.set(y, x, color);
+        }
+        else {
+            image.set(x, y, color);
+
+        }
+
+        error -= dy;
+        if (error <= 0) {
+            y += y1 > y0 ? 1: -1;
+            error += dx;
+        }
+
+    }
+
+}
+
 int main(int argc, char** argv) {
-	TGAImage image(100, 100, TGAImage::RGB);
-	image.set(52, 41, red);
-	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-	image.write_tga_file("output.tga");
-	return 0;
+    TGAImage image(100, 100, TGAImage::RGB);
+
+    line(13, 20, 80, 40, image, white); 
+    line(20, 13, 40, 80, image, red); 
+    line(80, 40, 13, 20, image, red);
+
+    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    image.write_tga_file("output.tga");
+    return 0;
 }
 
